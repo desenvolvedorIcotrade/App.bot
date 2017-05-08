d4fb70d0151f3ae22b3119a6cba6a1cf79f7f8b9
Grey Baker <greysteil@gmail.com>
2017-05-08 18:41:08 +0100

    Less cruft

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,5 @@
 $(document).ready(function() {
 
-	/***************** Waypoints ******************/
-
-	$('.wp1').waypoint(function() {
-		$('.wp1').addClass('animated fadeInUp');
-	}, {
-		offset: '75%'
-	});
-	$('.wp2').waypoint(function() {
-		$('.wp2').addClass('animated fadeInUp');
-	}, {
-		offset: '75%'
-	});
-	$('.wp3').waypoint(function() {
-		$('.wp3').addClass('animated fadeInRight');
-	}, {
-		offset: '75%'
-	});
-
-	/***************** Initiate Flexslider ******************/
-	$('.flexslider').flexslider({
-		animation: "slide"
-	});
-
-	/***************** Initiate Fancybox ******************/
-
-	$('.single_image').fancybox({
-		padding: 4,
-	});
-
-	/***************** Tooltips ******************/
-    $('[data-toggle="tooltip"]').tooltip();
-
 	/***************** Nav Transformicon ******************/
 
 	/* When user clicks the Icon */
@@ -80,24 +48,4 @@ $(document).ready(function() {
 			}
 		});
 	});
-	/***************** Smooth Scrolling ******************/
-
-	$(function() {
-
-		$('a[href*=#]:not([href=#])').click(function() {
-			if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-				if (target.length) {
-					$('html,body').animate({
-						scrollTop: target.offset().top
-					}, 300);
-					return false;
-				}
-			}
-		});
-
-	});
-
 });
