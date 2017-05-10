649cf66ecb44478c9b0558614263b2269c33b2b7
Harry Marr <harry.marr@gmail.com>
2017-05-10 16:52:54 +0100

    Even better header

--- js/scripts.js
+++ js/scripts.js
@@ -15,22 +15,22 @@ $(document).ready(function() {
   });
 
   /***************** Header BG Scroll ******************/
-  function setHeaderStyles() {
-    var scroll = $(window).scrollTop();
 
-    if (scroll >= 20) {
-      $('section.navigation').addClass('fixed');
-      $('header').css({
-        "border-bottom": "none",
-      });
-    } else {
-      $('section.navigation').removeClass('fixed');
-      $('header').css({
-        "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-      });
-    }
-  }
+  $(function() {
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
 
-  $(window).scroll(setHeaderStyles);
-  setHeaderStyles();
+      if (scroll >= 20) {
+        $('section.navigation').addClass('fixed');
+        $('header').css({
+          "border-bottom": "none",
+        });
+      } else {
+        $('section.navigation').removeClass('fixed');
+        $('header').css({
+          "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
+        });
+      }
+    });
+  });
 });
